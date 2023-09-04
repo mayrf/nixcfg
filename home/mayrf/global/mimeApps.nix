5ac773645481b25ad820c8c155cdda35134178b5
mayrf <70516376+mayrf@users.noreply.github.com>
2023-09-04 17:10:02 +0200

    Update mimeapps.list file

--- home/mayrf/global/mimeApps.nix
+++ home/mayrf/global/mimeApps.nix
@@ -5,5 +5,6 @@
     "application/pdf" = "org.pwmt.zathura.desktop";
     "video/x-msvideo" = "vlc.desktop";
     "x-scheme-handler/postman" = "Postman.desktop";
+    "x-scheme-handler/msteams" = "teams.desktop";
   };
 }
