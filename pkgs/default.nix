d1c77b98724bbf27b069375e7c811953fcc7c4e1
mayrf <70516376+mayrf@users.noreply.github.com>
2024-08-09 08:23:04 +0200

    Minor kcl changes

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,4 +2,24 @@
 
 { pkgs ? import <nixpkgs> { } }: rec {
   kcl-language-server = pkgs.callPackage ./kcl-language-server { };
+  kcl-lsp = pkgs.kclvm.overrideAttrs {
+    postInstall = "";
+
+    # meta = with lib; {
+    #   description = "A lsp-server for the KCL programming language";
+    #   homepage = "https://github.com/kcl-lang/lsp";
+    #   license = licenses.asl20;
+    #   platforms = platforms.linux ++ platforms.darwin;
+    #   maintainers = with maintainers; [ selfuryon peefy ];
+    # };
+    buildPhase = ''
+      echo "Building the application"
+      cargo build --package kcl-language-server --release
+    '';
+
+    installPhase = ''
+      mkdir -p $out/bin
+      cp target/release/kcl-language-server $out/bin/
+    '';
+  };
 }
--- pkgs/kcl-language-server/default.nix
+++ pkgs/kcl-language-server/default.nix
@@ -66,25 +66,4 @@ rustPlatform.buildRustPackage rec {
     cp target/release/kcl-language-server $out/bin/
   '';
 
-  # vendorHash = "sha256-Xv8Tfq9Kb1xGFCWZQwBFDX9xZW9j99td/DUb7jBtkpE=";
-
-  # ldflags = [ "-w -s" "-X=kcl-lang.io/cli/pkg/version.version=v${version}" ];
-
-  # subPackages = [ "cmd/kcl" ];
-
-  # env vars https://github.com/kcl-lang/kcl-go/blob/main/pkg/env/env.go#L29
-  # postFixup = ''
-  #    wrapProgram $out/bin/kcl \
-  #   --set PATH ${lib.makeBinPath [ kclvm_cli ]} \
-  #   --set KCL_LIB_HOME ${lib.makeLibraryPath [ kclvm ]} \
-  #   --set KCL_GO_DISABLE_INSTALL_ARTIFACT false \
-  # '';
-
-  # postInstall = ''
-  #   installShellCompletion --cmd kcl \
-  #     --bash <($out/bin/kcl completion bash) \
-  #     --fish <($out/bin/kcl completion fish) \
-  #     --zsh <($out/bin/kcl completion zsh)
-  # '';
-
 }
