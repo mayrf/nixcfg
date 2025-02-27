595c0ba7a538861041850e9cd2baf36203e274d7
mayrf <70516376+mayrf@users.noreply.github.com>
2025-02-27 20:18:26 +0100

    Add tree-sitter

--- modules/home-manager/emacs/vanilla/init.el
+++ modules/home-manager/emacs/vanilla/init.el
@@ -1293,10 +1293,37 @@ For how the context is retrieved, see `my-denote-region-get-source-reference'."
     (magit-reset-soft "HEAD~1"))
   )
 
+;; (setq treesit-language-source-alist
+;;    '((bash "https://github.com/tree-sitter/tree-sitter-bash")
+;;      (cmake "https://github.com/uyha/tree-sitter-cmake")
+;;      (css "https://github.com/tree-sitter/tree-sitter-css")
+;;      (elisp "https://github.com/Wilfred/tree-sitter-elisp")
+;;      (go "https://github.com/tree-sitter/tree-sitter-go")
+;;      (html "https://github.com/tree-sitter/tree-sitter-html")
+;;      (javascript "https://github.com/tree-sitter/tree-sitter-javascript" "master" "src")
+;;      (json "https://github.com/tree-sitter/tree-sitter-json")
+;;      (make "https://github.com/alemuller/tree-sitter-make")
+;;      (markdown "https://github.com/ikatyang/tree-sitter-markdown")
+;;      (python "https://github.com/tree-sitter/tree-sitter-python")
+;;      (toml "https://github.com/tree-sitter/tree-sitter-toml")
+;;      (tsx "https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src")
+;;      (typescript "https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src")
+;;      (yaml "https://github.com/ikatyang/tree-sitter-yaml")))
+
+(electric-pair-mode)
+
 (use-package direnv
  :config
  (direnv-mode))
 
-;; (use-package go-ts-mode
-;;   :mode ("\\.go" . go-ts-mode)
-;;   :hook ((go-ts-mode . eglot)))
+(use-package editorconfig
+  :config
+  (editorconfig-mode 1))
+
+(use-package go-ts-mode
+  :ensure nil
+  :mode ("\\.go\\'" . go-ts-mode)
+  :hook ((go-ts-mode . eglot-ensure))
+  ;; :config
+  ;; (add-hook 'go-ts-mode-hook 'eglot-ensure)
+  )
