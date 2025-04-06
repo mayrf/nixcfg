6897c87ae58c30199de1ac49f8ac7b80d9c9f97e
mayrf <70516376+mayrf@users.noreply.github.com>
2025-04-06 22:54:15 +0200

    Fix zsh for radium

--- home/mayrf/radium.nix
+++ home/mayrf/radium.nix
@@ -1,8 +1,20 @@
 { inputs, ... }:
 
 {
-  imports = [ ./global ./linux ./features/terminal/kitty.nix ];
+  imports = [
+    ./global
+    ./linux
+    ./features/terminal/kitty.nix
+    ../features/cli
+    ../features/desktop
+  ];
 
+  features = {
+    cli = {
+      zsh.enable = true;
+      fzf.enable = true;
+    };
+  };
   lf.enable = true;
   # vscode.enable = true;
   emacs.enable = true;
