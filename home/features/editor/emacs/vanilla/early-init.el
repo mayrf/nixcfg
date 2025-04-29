d88fd2c4db1d03d0a3415583e672ea63226b7d47
mayrf <70516376+mayrf@users.noreply.github.com>
2025-04-29 09:12:45 +0200

    Add emacs startup optimisations

--- home/features/editor/emacs/vanilla/early-init.el
+++ home/features/editor/emacs/vanilla/early-init.el
@@ -9,3 +9,27 @@
 (tool-bar-mode -1)                 ; disable toolbar
 (tooltip-mode -1)                  ; disable tooltips
 (menu-bar-mode -1)                 ; disable menubar
+
+;; Temporarily increase the garbage collection threshold.  These
+;; changes help shave off about half a second of startup time.  The
+;; `most-positive-fixnum' is DANGEROUS AS A PERMANENT VALUE.  See the
+;; `emacs-startup-hook' a few lines below for what I actually use.
+(setq gc-cons-threshold most-positive-fixnum
+      gc-cons-percentage 0.5)
+
+;; Same idea as above for the `file-name-handler-alist' and the
+;; `vc-handled-backends' with regard to startup speed optimisation.
+;; Here I am storing the default value with the intent of restoring it
+;; via the `emacs-startup-hook'.
+(defvar prot-emacs--file-name-handler-alist file-name-handler-alist)
+(defvar prot-emacs--vc-handled-backends vc-handled-backends)
+
+(setq file-name-handler-alist nil
+      vc-handled-backends nil)
+
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (setq gc-cons-threshold (* 1000 1000 8)
+                  gc-cons-percentage 0.1
+                  file-name-handler-alist prot-emacs--file-name-handler-alist
+                  vc-handled-backends prot-emacs--vc-handled-backends)))
