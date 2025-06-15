feea43de32403388df96bdea0295af44bdcc5999
mayrf <70516376+mayrf@users.noreply.github.com>
2025-06-15 22:37:47 +0200

    Small tweaks

--- home/features/desktop/hyprland.nix
+++ home/features/desktop/hyprland.nix
@@ -90,12 +90,12 @@ in {
           # timeout = 900;
           # timeout = 90;
           # on-timeout = "hyprlock";
-          timeout = 300; # 5min
+          timeout = 600; # 10min
           on-timeout =
             "loginctl lock-session"; # lock screen when timeout has passed
         }
         {
-          timeout = 330; # 5.5min
+          timeout = 900; # 15min
           on-timeout =
             "hyprctl dispatch dpms off"; # screen off when timeout has passed
           on-resume =
--- hosts/yttrium/hardware-configuration-additional.nix
+++ hosts/yttrium/hardware-configuration-additional.nix
@@ -18,18 +18,18 @@
   # Power/cpu savings
   # https://nixos.wiki/wiki/Laptop
   services.auto-cpufreq.enable = true;
-  services.auto-cpufreq.settings = {
-    battery = {
-      governor = "powersave";
-      turbo = "never";
-      energy_performance_preference = "power";
-    };
-    charger = {
-      governor = "powersave";
-      turbo = "never";
-      energy_performance_preference = "power";
-    };
-  };
+  # services.auto-cpufreq.settings = {
+  #   battery = {
+  #     governor = "powersave";
+  #     turbo = "never";
+  #     energy_performance_preference = "power";
+  #   };
+  #   charger = {
+  #     governor = "powersave";
+  #     turbo = "never";
+  #     energy_performance_preference = "power";
+  #   };
+  # };
   services.thermald.enable = true;
   services.system76-scheduler.settings.cfsProfiles.enable = true;
   hardware.graphics.enable32Bit = true;
