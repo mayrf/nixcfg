076b6805e144a40224f71cbb0e0166f85a0e5017
mayrf <70516376+mayrf@users.noreply.github.com>
2026-01-22 22:15:19 +0100

    Fix hyprland startup command

--- home/features/desktop/hyprland.nix
+++ home/features/desktop/hyprland.nix
@@ -22,17 +22,17 @@ in {
     }; programs = {
       fish.loginShellInit = ''
         if test (tty) = "/dev/tty1"
-          exec Hyprland &> /dev/null
+          exec start-hyprland &> /dev/null
         end
       '';
       zsh.loginExtra = ''
         if [ "$(tty)" = "/dev/tty1" ]; then
-          exec Hyprland &> /dev/null
+          exec start-hyprland &> /dev/null
         fi
       '';
      zsh.profileExtra = ''
         if [ "$(tty)" = "/dev/tty1" ]; then
-          exec Hyprland &> /dev/null
+          exec start-hyprland &> /dev/null
         fi
       '';
     };
