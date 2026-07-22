12b1f29127018060ddacac242b1946ffe06fb264
mayrf <70516376+mayrf@users.noreply.github.com>
2026-07-22 10:52:04 +0200

    Remove custom.el from gitignore

---
+++ modules/emacs/config/custom.el
@@ -0,0 +1,25 @@
+;;; -*- lexical-binding: t -*-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(agent-shell-anthropic-default-model-id "sonnet")
+ '(agent-shell-preferred-agent-config 'claude-code)
+ '(agent-shell-session-restore-verbosity 'full)
+ '(agent-shell-tool-use-expand-by-default t)
+ '(agent-shell-user-message-expand-by-default t)
+ '(auto-save-visited-mode t)
+ '(package-selected-packages
+   '(agent-shell avy cape corfu direnv dirvish eat ef-themes ellama
+		 embark-consult evil forge helpful jinx load-env-vars
+		 magit marginalia nix-ts-mode no-littering orderless
+		 org-cliplink org-jira org-noter org-roam pdf-tools
+		 popper popper-mode prescient vertico vterm web-mode
+		 yasnippet-snippets)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
